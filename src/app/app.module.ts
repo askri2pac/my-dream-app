e73caba762742a15060d6dc14dccac5da753051d
root <nassimaskri@gmail.com>
2017-12-28 23:43:21 +0100

    angular-route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule, ROUTES, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
-
+export const ROUTES: Routes = [{ path:'login',component: LoginComponent}]
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(ROUTES),
   ],
   providers: [],
   bootstrap: [AppComponent]
